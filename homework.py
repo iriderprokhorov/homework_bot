5d3476b51e5d46e6b723df4d377e485ab9c9fec6
Nick Prokhorov <irider.pro@yandex.ru>
2022-02-19 15:21:23 +0300

    first release

--- homework.py
+++ homework.py
@@ -1,30 +1,30 @@
-# from black import err
-# from http import HTTPStatus
-
-# from asyncio.log import logger
 import logging
 import requests
 import os
+import sys
 import telegram
 import time
 
 from dotenv import load_dotenv
+from http import HTTPStatus
 
 
 load_dotenv()
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter(
+    '%(asctime)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
-# Здесь задана глобальная конфигурация для логирования
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s',
-)
 
 PRACTICUM_TOKEN = os.getenv('P_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('CHAT_ID')
 
-RETRY_TIME = 6
+RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
@@ -42,9 +42,9 @@ def send_message(bot, message):
         bot.send_message(TELEGRAM_CHAT_ID, message)
     except Exception as error:
         message = f'Can t send message to telegram: {error}'
-        logging.error(message)
+        logger.error(message)
     else:
-        logging.info('successful send message to telegram')
+        logger.info('successful send message to telegram')
 
 
 def get_api_answer(current_timestamp):
@@ -57,12 +57,13 @@ def get_api_answer(current_timestamp):
         homework_response = requests.get(
             url=ENDPOINT, headers=HEADERS, params=params
         )
-    except requests.exceptions.RequestException as error:
-        message = f"http response is not 200: {error}"
-        logging.error(message)
+    except requests.ConnectionError as error:
+        message = f'connection error: {error}'
+        logger.error(message)
     else:
-        if homework_response.status_code != 200:
-            logging.error("Http response is not 200")
+        if homework_response.status_code != HTTPStatus.OK.value:
+            logger.error('Http response is not 200')
+            raise requests.HTTPError('Http response is not 200')
         else:
             return homework_response.json()
 
@@ -70,74 +71,73 @@ def get_api_answer(current_timestamp):
 def check_response(response):
     """function validation response"""
     try:
-        checked_response = response['homeworks']
+        homeworks = response['homeworks']
     except KeyError as error:
         message = f'response is not consist necessery keys {error}'
-        logging.error(message)
+        logger.error(message)
+        raise KeyError(message)
     else:
-        if not isinstance(checked_response, list):
-            logging.error("homeworks is wrong type")
-        if not bool(checked_response):
-            logging.error("homeworks is empty")
-        else:
-            return checked_response
+        if not isinstance(homeworks, list):
+            logger.error('homeworks is wrong type')
+            raise TypeError('homeworks is wrong type')
+        if not homeworks:
+            logger.error('homeworks is empty')
+            raise KeyError('homeworks is empty')
+        return homeworks
 
 
 def parse_status(homework):
     """Check homework status"""
     try:
-        homework_name = homework.get('homework_name')
-        homework_status = homework.get('status')
+        homework_name = homework['homework_name']
+        homework_status = homework['status']
     except KeyError as error:
-        # raise KeyError('ststus is not doc')
         message = f'response is not consist necessery keys {error}'
-        logging.error(message)
+        logger.error(message)
+        raise KeyError('response is not consist necessery keys')
     else:
-        verdict = HOMEWORK_STATUSES[homework_status]
-        return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+        if homework_status in HOMEWORK_STATUSES:
+            verdict = HOMEWORK_STATUSES[homework_status]
+            return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+        else:
+            logger.error('homework status is not defined')
+            raise Exception('homework status is not defined')
 
 
 def check_tokens():
-    """Check token available"""
-    try:
-        for token in (PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID):
-            if not token:
-                raise Exception('Token is not available')
-    except Exception as error:
-        message = f'Token is not available: {error}'
-        logging.critical(message)
-        # send_message(bot, message)
-    else:
-        return True
+    """Check token availability"""
+    for token in (PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID):
+        if not token:
+            logger.critical('token is not available')
+            return False
+    return True
 
 
 def main():
     """Main module"""
-    try:
-        check_tokens() is True
-    except Exception as error:
-        message = f'Token is not available: {error}'
-        logging.critical(message)
-    else:
+    if check_tokens() is True:
         bot = telegram.Bot(token=TELEGRAM_TOKEN)
         current_timestamp = int(time.time())
+    else:
+        return
 
-    cache = ()
 
     while True:
         try:
             response = get_api_answer(current_timestamp)
+            homeworks = check_response(response)
+            
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            logging.error(message)
-            time.sleep(RETRY_TIME)
+            logger.error(message)
         else:
-            if len(cache) == 0:
-                cache = parse_status(response)
+            verdict = parse_status(homeworks[0])           
+            if not verdict:
+                send_message(bot, verdict)
             else:
-                if cache == parse_status(response):
-                    send_message(bot, parse_status(response))
-                    print(parse_status(response))
+                logger.debug("not change")
+            current_timestamp = response['current_date']
+        finally:
             time.sleep(RETRY_TIME)
 
 
