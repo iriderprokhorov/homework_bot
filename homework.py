b09321ac852058771d5fc47243ceac02b5468d6e
Nick Prokhorov <irider.pro@yandex.ru>
2022-02-20 15:38:29 +0300

    fix few function

--- homework.py
+++ homework.py
@@ -1,4 +1,5 @@
 import logging
+import json
 import os
 import requests
 import sys
@@ -13,25 +14,24 @@ load_dotenv()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter(
-    '%(asctime)s %(levelname)s %(message)s')
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
-PRACTICUM_TOKEN = os.getenv('P_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('CHAT_ID')
+PRACTICUM_TOKEN = os.getenv("P_TOKEN")
+TELEGRAM_TOKEN = os.getenv("TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("CHAT_ID")
 
 RETRY_TIME = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+ENDPOINT = "https://practicum.yandex.ru/api/user_api/homework_statuses/"
+HEADERS = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
 
 
 HOMEWORK_STATUSES = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.',
+    "approved": "Работа проверена: ревьюеру всё понравилось. Ура!",
+    "reviewing": "Работа взята на проверку ревьюером.",
+    "rejected": "Работа проверена: у ревьюера есть замечания.",
 }
 
 
@@ -40,75 +40,86 @@ def send_message(bot, message):
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
     except Exception as error:
-        message = f'Can t send message to telegram: {error}'
+        message = f"Can t send message to telegram: {error}"
         logger.error(message)
     else:
-        logger.info('successful send message to telegram')
+        logger.info("successful send message to telegram")
 
 
 def get_api_answer(current_timestamp):
     """Function getting response from api."""
     timestamp = current_timestamp or int(time.time())
     # 1642412129
-    params = {'from_date': timestamp}
+    params = {"from_date": timestamp}
 
     try:
         homework_response = requests.get(
             url=ENDPOINT, headers=HEADERS, params=params
         )
     except requests.ConnectionError as error:
-        message = f'connection error: {error}'
+        message = f"connection error: {error}"
         logger.error(message)
+    if homework_response.status_code != HTTPStatus.OK.value:
+        logger.error(homework_response.status_code)
+        raise requests.HTTPError("Http response is not 200")
     else:
-        if homework_response.status_code != HTTPStatus.OK.value:
-            logger.error('Http response is not 200')
-            raise requests.HTTPError('Http response is not 200')
-        else:
-            return homework_response.json()
+        try:
+            serialized_response = homework_response.json()
+        except json.decoder.JSONDecodeError as error:
+            message = f"can t serialize to json: {error}"
+            logger.error(message)
+        return serialized_response
 
 
 def check_response(response):
     """Function validation response."""
     try:
-        homeworks = response['homeworks']
+        homeworks = response["homeworks"]
     except KeyError as error:
-        message = f'response is not consist necessery keys {error}'
+        message = f"response is not consist necessery keys {error}"
         logger.error(message)
         raise KeyError(message)
     else:
         if not isinstance(homeworks, list):
-            logger.error('homeworks is wrong type')
-            raise TypeError('homeworks is wrong type')
+            logger.error("homeworks is wrong type")
+            raise TypeError("homeworks is wrong type")
         if not homeworks:
             logger.error(homeworks)
-            raise KeyError('homeworks is empty')
+            raise KeyError("homeworks is empty")
         return homeworks
 
 
 def parse_status(homework):
     """Check homework status."""
     try:
-        homework_name = homework['homework_name']
-        homework_status = homework['status']
+        homework_name = homework["homework_name"]
+        homework_status = homework["status"]
     except KeyError as error:
-        message = f'response is not consist necessery keys {error}'
+        message = f"response is not consist necessery keys {error}"
         logger.error(message)
-        raise KeyError('response is not consist necessery keys')
+        raise KeyError("response is not consist necessery keys")
     else:
         if homework_status in HOMEWORK_STATUSES:
             verdict = HOMEWORK_STATUSES[homework_status]
-            return (f'Изменился статус проверки работы'
-                    f' "{homework_name}". {verdict}')
+            return (
+                f"Изменился статус проверки работы"
+                f' "{homework_name}". {verdict}'
+            )
         else:
-            logger.error('homework status is not defined')
-            raise Exception('homework status is not defined')
+            logger.error(homework_status)
+            raise Exception("homework status is not defined")
 
 
 def check_tokens():
     """Check token availability."""
-    for token in (PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID):
-        if not token:
-            logger.critical('token is not available')
+    dict_token = {
+        "practicum_token": PRACTICUM_TOKEN,
+        "telegram_token": TELEGRAM_TOKEN,
+        "telecgram_chat_id": TELEGRAM_CHAT_ID,
+    }
+    for token, value in dict_token.items():
+        if not value:
+            logger.critical(f"{token} is not available")
             return False
     return True
 
@@ -116,12 +127,16 @@ def check_tokens():
 def main():
     """Main module."""
     if check_tokens() is True:
-        bot = telegram.Bot(token=TELEGRAM_TOKEN)
+        try:
+            bot = telegram.Bot(token=TELEGRAM_TOKEN)
+        except Exception as error:
+            message = f"BOT is not initialized: {error}"
+            logger.error(message)
         current_timestamp = int(time.time()) - 36000
     else:
         return
 
-    last_result = ''
+    last_result = ""
 
     while True:
         try:
@@ -129,12 +144,12 @@ def main():
             homeworks = check_response(response)
             verdict = parse_status(homeworks[0])
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
+            message = f"Сбой в работе программы: {error}"
             logger.error(message)
         else:
             logger.info(verdict)
             if last_result == verdict:
-                logger.info('not change')
+                logger.info("not change")
             else:
                 last_result = verdict
                 send_message(bot, verdict)
@@ -142,5 +157,5 @@ def main():
             time.sleep(RETRY_TIME)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
